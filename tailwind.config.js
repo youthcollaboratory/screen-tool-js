93d3e12a8b875a3408434aca7e2d6fb3a9c8e574
youthcollaboratory <communication@youthcollaboratory.org>
2025-04-10 10:08:15 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,18 +12,14 @@ module.exports = {
         'yc-green-dark': '#009e85',
       },
       keyframes: {
-        flash: {
-          '0%': { backgroundColor: '#ffff99' },
-          '100%': { backgroundColor: 'transparent' },
-        },
-        blink: {
-          '0%, 100%': { backgroundColor: 'transparent' },
-          '25%, 75%': { backgroundColor: '#ffff99' },
+        pulseHighlight: {
+        '0%, 100%': { backgroundColor: 'transparent' },
+        '50%': { backgroundColor: '#ffff99' },
         },
       },
       animation: {
         'flash-once': 'flash 1s ease-out',
-        'blink-sharp': 'blink 0.2s steps(1, start) 3',
+        'pulse-soft': 'pulseHighlight 1.2s ease-in-out 2',
       },
     },
   },
