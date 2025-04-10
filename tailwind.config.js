944a806bfae7a09321a549c876ddc82e7eb7cb04
youthcollaboratory <communication@youthcollaboratory.org>
2025-04-10 08:41:33 -0400

    Update tailwind.config.js
    added custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'yc-blue': '#0085ad',
+        'yc-blue-dark': '#007295',
+        'yc-green': '#00b398',
+        'yc-green-dark': '#009e85',
+      },
+    },
   },
   plugins: [],
 }
