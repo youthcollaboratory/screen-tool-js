c04842669e0d32508edf69504f88fb94421731b0
Youth Collaboratory <communication@youthcollaboratory.org>
2025-06-17 13:09:06 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,39 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Script from 'next/script';
+import * as ga from '../lib/ga4'; // We’ll create this file in Step 2
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      {/* GA4 Script Loader */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-ZDE09SGVV3"
+        strategy="afterInteractive"
+      />
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){ dataLayer.push(arguments); }
+          gtag('js', new Date());
+          gtag('config', 'G-ZDE09SGVV3', { send_page_view: false });
+        `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
